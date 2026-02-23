7c9cced7981c3b0e9921b68d395a143820714fca
perkee <alex.perkins@avetta.com>
2026-02-23 15:56:51 -0700

    Update formula with real SHA256 hashes for all platforms

--- Formula/elm-format.rb
+++ Formula/elm-format.rb
@@ -7,8 +7,13 @@ class ElmFormat < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/perkee/elm-format/releases/download/v0.8.7-teleport/elm-format-v0.8.7-teleport-arm64-apple-darwin.tar.gz"
-      sha256 "80e221aeda25a142335805f8fc5129cfef3445c6870f8d72835a974eab47fb3d"
+      sha256 "bc6a2ca8eec8fda0d2164de75d514df6e291a12de5eed92fd13aafb4ec544c41"
     end
+  end
+
+  on_linux do
+    url "https://github.com/perkee/elm-format/releases/download/v0.8.7-teleport/elm-format-v0.8.7-teleport-x86_64-linux.tar.gz"
+    sha256 "12eb671c8f3345fb2e302b30cfe7724dffde06ba02063e7583b79529ab9363b3"
   end
 
   def install
